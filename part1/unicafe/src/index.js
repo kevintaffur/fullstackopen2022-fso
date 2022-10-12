e780365c9113d5b2c42cf59765b6dcf0c789b985
Kevin <86487929+tar83@users.noreply.github.com>
2022-10-11 23:06:45 -0500

    exercise 1.6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Feedback = ({ addGoodVote, addNeutralVote, addBadVote }) => (
+  <>
+    <h1>give feedback</h1>
+    <div>
+      <Button handleClick={addGoodVote} text="good" />
+      <Button handleClick={addNeutralVote} text="neutral" />
+      <Button handleClick={addBadVote} text="bad" />
+    </div>
+  </>
+)
+
+const Statistics = ({ good, neutral, bad }) => (
+  <>
+    <h2>statistics</h2>
+    <div>
+      good {good}
+      <br />
+      neutral {neutral}
+      <br />
+      bad {bad}
+    </div>
+  </>
+)
+
+const App = () => {
+  // Each button has his own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const addGoodVote = () => setGood(good + 1);
+  const addNeutralVote = () => setNeutral(neutral + 1);
+  const addBadVote = () => setBad(bad + 1);
+
+  return (
+    <>
+      <Feedback addGoodVote={addGoodVote} addNeutralVote={addNeutralVote} addBadVote={addBadVote} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
